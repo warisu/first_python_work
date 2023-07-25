a04995be9f8e3d8d6759490ef935ec577a91abf4
warisu <abdullahiabdulwaris@gmail.comgit config --global user.email abdullahiabdulwaris@gmail.com>
2023-07-25 13:37:20 +0100

    initial commit

---
+++ .venv/name.py
@@ -0,0 +1,17 @@
+# places_of_world = ["mexico", "japan", "us", "uk", "singapore", "saudi arabia", "iran", "sao tome and principle"];
+# print(places_of_world);
+
+# transport_modes = ['cars', 'lorry', 'bus', 'airplane', 'train'];
+# print("one of the most expensive" , transport_modes[0])
+
+my_foods = ['pizza', 'falfel', 'carrot cake']
+friend_foods = my_foods[:]
+
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+
+print("My favourite foods are:")
+print(my_foods)
+
+print("\nMy friend's favourite foods are:")
+print(friend_foods)
